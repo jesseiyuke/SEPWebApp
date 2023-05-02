416f32723dc3e3cba108ece2bf3c851dd1f88272
Jesse Iyuke <jes.iyuke@gmail.com>
2023-05-02 11:59:49 +0200

    Add project files.

---
+++ SEPWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SEPWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
