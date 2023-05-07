1fcbcad5f4ba41101787c1548648effd64e00270
Jesse Iyuke <jes.iyuke@gmail.com>
2023-05-07 23:19:56 +0200

    Create JobPost Index view

---
+++ SEPWebApp/Controllers/JobPostController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEPWebApp.Data;
+using SEPWebApp.Models;
+
+namespace SEPWebApp.Controllers
+{
+    public class JobPostController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public JobPostController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<JobPost> objJobPostList = _db.JobPost;
+            return View(objJobPostList);
+        }
+    }
+}
