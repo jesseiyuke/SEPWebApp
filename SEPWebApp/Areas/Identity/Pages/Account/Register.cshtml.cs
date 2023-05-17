5d5822e4d36368799b26e39d01430153a203d2cc
Jesse Iyuke <jes.iyuke@gmail.com>
2023-05-17 18:18:47 +0200

    Added Drop-down Roles

--- SEPWebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SEPWebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using SEP.Models;
 using SEP.Utility;
@@ -102,6 +104,11 @@ namespace SEPWebApp.Areas.Identity.Pages.Account
             public string? EmailAndUsername { get; set; }
             public int? Telephone { get; set; }
             public int? Cellphone { get; set; }
+
+            public string? Role { get; set; }
+
+            [ValidateNever]
+            public IEnumerable<SelectListItem> RoleList { get; set; }
         }
 
 
@@ -116,6 +123,15 @@ namespace SEPWebApp.Areas.Identity.Pages.Account
 
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            Input = new InputModel()
+            {
+                RoleList = _roleManager.Roles.Select(x => x.Name).Select(i => new SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
@@ -141,6 +157,15 @@ namespace SEPWebApp.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    if (Input.Role == null) //if null it will be student
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.Role_Student);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, Input.Role);
+                    }
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
