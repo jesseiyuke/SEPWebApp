8be052534ceb50b3f94a1af91b0e08be43617f8e
Jesse Iyuke <jes.iyuke@gmail.com>
2023-05-20 19:45:55 +0200

    Redirecting Employer

---
+++ SEPWebApp/Areas/Home/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SEP.Models;
+using System.Diagnostics;
+
+namespace SEPWebApp.Areas.Home.Controllers
+{
+    [Area("Home")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public HomeController(ILogger<HomeController> logger, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+
+            if (_signInManager.IsSignedIn(User))
+            {
+
+                if (User.IsInRole("Employer"))
+                {
+                    return RedirectToAction("Index", "Employer", new { area = "Employer" });
+                }
+            }
+
+            return RedirectToAction("Login", "Account", new { area = "Identity" });
+        }
+
+
+
+        /*        public IActionResult Index()
+                {
+                    return view();
+                }*/
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
