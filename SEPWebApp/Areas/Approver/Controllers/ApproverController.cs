36c83dbfff7b14bd2861d5fc58329fb25b1dadb7
Jesse Iyuke <jes.iyuke@gmail.com>
2023-06-07 14:52:09 +0200

    Added Approver MVC

---
+++ SEPWebApp/Areas/Approver/Controllers/ApproverController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SEP.DataAccess.Repository.IRepository;
+using SEP.Models;
+using SEP.Models.ViewModels;
+using SEP.Utility;
+using System.Data;
+
+namespace SEPWebApp.Areas.Employer.Controllers
+{
+    [Area("Approver")]
+    [Authorize(Roles = SD.Role_Approver)]
+    public class ApproverController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ApproverController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ReviewExternalEmployers()
+        {
+            return View();
+        }
+
+        public IActionResult ReviewPosts()
+        {
+            return View();
+        }
+        public IActionResult Stats()
+        {
+            return View();
+        }
+
+    }
+}
